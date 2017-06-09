9c6e929be58575511c20beec33ffa28aa2cc633b
rubaktechie <rubaktechie@gmail.com>
2017-06-09 09:03:36 +0000

    completed home page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<html lang="en">
+
+<head>
+    <link rel="icon" href="../resources/assets/images/fav.png   " type="image/gif" sizes="16x16"> 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="rubaktechie" content="rubaktechie">
+    <title>Rubaktechie</title>
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../resources/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../resources/assets/css/icons.css" rel="stylesheet">
+    <link href="../resources/assets/css/portfolio-item.css" rel="stylesheet">
+
+</head>
+<body style="margin-top: 5%;margin-left: 14%;margin-right: 15%">
+    <div class="container" >
+        <div class="row">
+            <div class="col-md-4">
+                <h1 class="page-header">Rubaktechie<br/>
+                <small> &nbsp;&nbsp;&nbsp;  fullstack developer</small>
+                </h1>
+                <h2>Hi there,,,</h2>
+                <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;To me, I am a person who is positive about every aspect of my life , say an optimist , who takes every experience as a lesson and follow what it taught me ,I am interested in web development as well as mobile app development , I love to work in a team as well as a individual , then I love hanging out with my friends .that’s all about me.</h4>
+                <a href="https://facebook.com/rubaktechie" target="_blank">
+                    <div class='icon social fb demopadding'><i class='fa fa-facebook'></i></div>      
+                </a>
+                <a href="https://twitter.com/@rubaktechie" target="_blank">
+                    <div class='icon social tw demopadding'><i class='fa fa-twitter'></i></div>    
+                </a>
+                <a href="https://linkedin.com/in/rubak-kumar/" target="_blank">
+                    <div class='icon social in demopadding'><i class='fa fa-linkedin'></i></div>
+                </a>
+                <a href="https://github.com/rubaktechie" target="_blank">
+                    <div class='icon social in demopadding'><i class='fa fa-github'></i></div>   
+                </a>
+            </div>
+            <div class="col-md-6">
+                <img src="../resources/assets/images/rubaktechie1.jpg" alt="" height="70%">
+            </div>
+        </div>
+    </div>
+    <script src="../resources/assets/js/jquery.js"></script>
+    <script src="../resources/assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
